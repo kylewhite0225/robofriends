110b8b7751821480900272c06f125d9588b17d5a
Kyle White <kylewhite0225@gmail.com>
2022-04-11 18:08:38 -0700

    Created application and updated App.js and related components.

---
+++ src/components/Scroll.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Scroll = (props) => {
+  // console.log(props);
+  return (
+    <div
+      style={{
+        overflowY: "scroll",
+        border: "5px solid darkblue",
+        height: "80vh",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default Scroll;
